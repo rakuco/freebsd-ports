685be679080ccf59fa5b40cf5348b1e20949b06c
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2015-03-07 00:22:55 +0200

    Sync with area51's trunk/area51/QT at r10630.

--- Mk/bsd.qt.mk
+++ Mk/bsd.qt.mk
@@ -297,7 +297,7 @@ _USE_QT4_ONLY=	accessible assistant-adp assistantclient codecs-cn codecs-jp \
 				qtestlib qvfb rcc uic uic3 xmlpatterns-tool
 
 _USE_QT5_ONLY=	buildtools concurrent core graphicaleffects \
-				paths printsupport qdbus qdoc qev qml quick \
+				paths phonon4 phonon4-gst printsupport qdbus qdoc qev qml quick \
 				quickcontrols serialport uitools widgets x11extras
 
 accessible_PORT=	accessibility/${_QT_RELNAME}-accessible
@@ -411,8 +411,14 @@ pixeltool_PATH=		${QT_BINDIR}/pixeltool
 phonon_PORT=		multimedia/phonon
 phonon_PATH=		${QT_LIBDIR}/libphonon.so
 
+phonon4_PORT=		multimedia/${_QT_RELNAME}-phonon4
+phonon4_PATH=		${QT_LIBDIR}/libphonon4${_QT_RELNAME}.so
+
 phonon-gst_PORT=	multimedia/phonon-gstreamer
 phonon-gst_PATH=	${QT_PLUGINDIR}/phonon_backend/libphonon_gstreamer.so
+
+phonon4-gst_PORT=	multimedia/${_QT_RELNAME}-phonon4-gstreamer
+phonon4-gst_PATH=	${QT_PLUGINDIR}/phonon4${_QT_RELNAME}_backend/libphonon_gstreamer.so
 
 porting_PORT=		devel/${_QT_RELNAME}-porting
 porting_PATH=		${QT_BINDIR}/qt3to4
